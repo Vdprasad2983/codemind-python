10ed5aaf110a26c66eb8daa805c76b1398ad0d0e
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-06-06 10:34:06 +0530

    n=input().lower() a=n.split(" ") d=[] for i in a:     for j in i:         if n.count(j)==1:             d.append(j) for i in (sorted(d)):     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,8 +1,9 @@
-n=input()
-x=n.lower()
-lst=sorted(set(x))
-for i in lst:
-    if i==" ":
-        continue
-    else:
-        print(i,end="")
+n=input().lower()
+a=n.split(" ")
+d=[]
+for i in a:
+    for j in i:
+        if n.count(j)==1:
+            d.append(j)
+for i in (sorted(d)):
+    print(i,end="")
