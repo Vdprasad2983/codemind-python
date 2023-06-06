da4811dc3efa1e2d300dbc4d551497823686f997
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-06-06 09:45:27 +0530

    n1=input().lower() n2=input().lower() s1='' s2='' for i in n1:     if i!=" ":         s1+=i for j in n2:     if j!=" ":         s2+=j s='' for k in s2:     if k in s1 and k not in s:         s+=k a=(sorted(s)) if len(a)!=0:     for i in a:         print(i,end="") else:     print(-1)

--- common_characters.py
+++ common_characters.py
@@ -1,12 +1,20 @@
-s1=input().lower()
-s2=input().lower()
-a=""
-for i in s2:
-    if i in s1 and i not in a and i!=" ":
-        a+=i
-k=sorted(a)
-if len(k)!=0:
-    for j in k:
-        print(j,end="")
+n1=input().lower()
+n2=input().lower()
+s1=''
+s2=''
+for i in n1:
+    if i!=" ":
+        s1+=i
+for j in n2:
+    if j!=" ":
+        s2+=j
+s=''
+for k in s2:
+    if k in s1 and k not in s:
+        s+=k
+a=(sorted(s))
+if len(a)!=0:
+    for i in a:
+        print(i,end="")
 else:
     print(-1)
