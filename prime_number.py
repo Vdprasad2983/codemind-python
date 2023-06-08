89ea47b0605538c8f1314120079d2ab3e93cd60d
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-06-08 12:06:07 +0530

    import math def prime(n):     if n==1:         return "not a prime"     s=int(math.sqrt(n))     for i in range(2,s+1):         if n%i==0:             return "not a prime"     return "prime" n=int(input()) print(prime(n))

--- prime_number.py
+++ prime_number.py
@@ -1,7 +1,11 @@
+import math
+def prime(n):
+    if n==1:
+        return "not a prime"
+    s=int(math.sqrt(n))
+    for i in range(2,s+1):
+        if n%i==0:
+            return "not a prime"
+    return "prime"
 n=int(input())
-for i in range(2,n-1):
-    if n%i==0:
-        print("not a prime")
-        break
-else:
-    print("prime")
+print(prime(n))
