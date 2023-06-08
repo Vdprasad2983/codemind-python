4c7fab44113b9c47abecf94dd991b52de2d9ac4f
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2023-06-08 13:47:30 +0530

    import math def prime(n):     if n==1:         return False     s=int(math.sqrt(n))     for i in range(2,s+1):         if n%i==0:             return False     return True def reverse(n):     s=str(n)     s_l=s[::-1]     return int(s_l) n=int(input()) if prime(n):     if prime(reverse(n)):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -2,21 +2,20 @@ import math
 def prime(n):
     if n==1:
         return False
-    sq=int(math.sqrt(n))
-    for i in range(2,sq+1):
+    s=int(math.sqrt(n))
+    for i in range(2,s+1):
         if n%i==0:
             return False
     return True
+def reverse(n):
+    s=str(n)
+    s_l=s[::-1]
+    return int(s_l)
 n=int(input())
-temp=n
-rev=0
-while n:
-    d=n%10
-    n//=10
-    rev=rev*10+d
-if prime(temp) and prime(rev):
-    print("circular prime")
-elif prime(temp):
-    print("prime but not a circular prime")
+if prime(n):
+    if prime(reverse(n)):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
     print("not prime")
